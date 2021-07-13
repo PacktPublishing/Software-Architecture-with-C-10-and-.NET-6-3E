1654533aa545a9d42b7ec14a0241c356ee4faa4d
gabriell <gabriel@smit.net.br>
2021-07-13 17:04:31 -0300

    Upload of code from chapter 02 renamed to 3rd edition and defined as .NET 6 version

---
+++ ch02/scalability-sample-3E/Pages/Privacy.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace scalability_sample_3E.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
