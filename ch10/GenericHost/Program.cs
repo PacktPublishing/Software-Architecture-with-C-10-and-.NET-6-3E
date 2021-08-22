8d4c0486012d31f6a66c65dffa2e7a4901c9084b
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2021-08-22 20:28:23 -0300

    Code update

--- ch10/GenericHost/Program.cs
+++ ch10/GenericHost/Program.cs
@@ -11,22 +11,20 @@ namespace GenericHost
     {
         public static void Main()
         {
-            var host = new HostBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<HostedService>();
-                    services.AddHostedService<MonitoringService>();
-                })
-                .ConfigureLogging((hostContext, configLogging) =>
-                {
-                    configLogging.AddConsole();
-                })
-                .Build();
-
-            host.Run();
-
+            CreateHostBuilder().Build().Run();
             Console.WriteLine("Host has terminated. Press any key to finish the App.");
             Console.ReadKey();
         }
+        public static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<HostedService>();
+                services.AddHostedService<MonitoringService>();
+            })
+            .ConfigureLogging((hostContext, configLogging) =>
+            {
+                configLogging.AddConsole();
+            });
     }
 }
