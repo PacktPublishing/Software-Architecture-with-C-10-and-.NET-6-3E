39df96cfc6dcd88378a0b7fbc2fc19a83b921ba3
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2021-08-22 20:08:13 -0300

    Chapter 10 Libs Update

---
+++ ch10/GenericHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace GenericHost
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<HostedService>();
+                    services.AddHostedService<MonitoringService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                })
+                .Build();
+
+            host.Run();
+
+            Console.WriteLine("Host has terminated. Press any key to finish the App.");
+            Console.ReadKey();
+        }
+    }
+}
