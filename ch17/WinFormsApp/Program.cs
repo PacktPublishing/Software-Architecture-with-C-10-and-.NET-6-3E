af07a5fcaefbf1b8d7f57e8524daf811ab91ddf4
Gabriel Lara Baptista <gabriel.l.baptista@gmail.com>
2021-10-03 19:57:37 -0300

    Beggining of chapter 17

---
+++ ch17/WinFormsApp/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
