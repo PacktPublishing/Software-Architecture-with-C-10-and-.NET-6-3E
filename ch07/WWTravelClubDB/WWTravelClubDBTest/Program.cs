b45d6fb0f452f3fc08c40adfe61ca24e11a52ba2
frankabbruzzese <francesco@dotnet-programming.com>
2022-01-11 21:20:47 +0100

    uodated code in chapter 7

--- ch07/WWTravelClubDB/WWTravelClubDBTest/Program.cs
+++ ch07/WWTravelClubDB/WWTravelClubDBTest/Program.cs
@@ -8,35 +8,35 @@ Console.ReadKey();
 
 var context = new LibraryDesignTimeDbContextFactory()
     .CreateDbContext();
-//var firstDestination = new Destination
-//{
-//    Name = "Florence",
-//    Country = "Italy",
-//    Packages = new List<Package>()
-//    {
-//        new Package
-//        {
-//            Name = "Summer in Florence",
-//            StartValidityDate = new DateTime(2019, 6, 1),
-//            EndValidityDate = new DateTime(2019, 10, 1),
-//            DurationInDays=7,
-//            Price=1000
-//        },
-//        new Package
-//        {
-//            Name = "Winter in Florence",
-//            StartValidityDate = new DateTime(2019, 12, 1),
-//            EndValidityDate = new DateTime(2020, 2, 1),
-//            DurationInDays=7,
-//            Price=500
-//        }
-//    }
-//};
-//context.Destinations.Add(firstDestination);
-//await context.SaveChangesAsync();
-//Console.WriteLine(
-//    $"DB populated: first destination id is {firstDestination.Id}");
-//Console.ReadKey();
+var firstDestination = new Destination
+{
+    Name = "Florence",
+    Country = "Italy",
+    Packages = new List<Package>()
+    {
+        new Package
+        {
+            Name = "Summer in Florence",
+            StartValidityDate = new DateTime(2019, 6, 1),
+            EndValidityDate = new DateTime(2019, 10, 1),
+            DurationInDays=7,
+            Price=1000
+        },
+        new Package
+        {
+            Name = "Winter in Florence",
+            StartValidityDate = new DateTime(2019, 12, 1),
+            EndValidityDate = new DateTime(2020, 2, 1),
+            DurationInDays=7,
+            Price=500
+        }
+    }
+};
+context.Destinations.Add(firstDestination);
+await context.SaveChangesAsync();
+Console.WriteLine(
+    $"DB populated: first destination id is {firstDestination.Id}");
+Console.ReadKey();
 
 var toModify = await context.Destinations
     .Where(m => m.Name == "Florence")
