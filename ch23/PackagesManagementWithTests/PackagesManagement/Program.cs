b06eb12bb235372acf0291f8cac54e24f44ce750
frankabbruzzese <francesco@dotnet-programming.com>
2022-01-16 10:43:16 +0100

    Updated chapter 23 code

--- ch23/PackagesManagementWithTests/PackagesManagement/Program.cs
+++ ch23/PackagesManagementWithTests/PackagesManagement/Program.cs
@@ -4,65 +4,62 @@ using PackagesManagement.Controllers;
 using PackagesManagementDB;
 using PackagesManagementDB.Extensions;
 using System.Globalization;
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbLayer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddAllQueries(typeof(ManagePackagesController).Assembly);
+builder.Services.AddAllCommandHandlers(typeof(ManagePackagesController).Assembly);
+builder.Services.AddAllEventHandlers(typeof(ManagePackagesController).Assembly);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static async Task Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddRazorPages();
-        builder.Services.AddDbLayer(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-        builder.Services.AddAllQueries(typeof(ManagePackagesController).Assembly);
-        builder.Services.AddAllCommandHandlers(typeof(ManagePackagesController).Assembly);
-        builder.Services.AddAllEventHandlers(typeof(ManagePackagesController).Assembly);
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
-        else
-        {
-            app.UseDeveloperExceptionPage();
-        }
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseAuthentication();
-        app.UseRouting();
-
-        app.UseAuthorization();
-        var ci = new CultureInfo("en-US");
-
-        // Configure the Localization middleware
-        app.UseRequestLocalization(new RequestLocalizationOptions
-        {
-            DefaultRequestCulture = new RequestCulture(ci),
-            SupportedCultures = new List<CultureInfo>
-                {
-                    ci,
-                },
-            SupportedUICultures = new List<CultureInfo>
-                {
-                    ci,
-                }
-        });
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-        await SeedDatabase(app.Services);
-        app.Run();
-    }
-
-    static async Task SeedDatabase(IServiceProvider serviceProvider)
-    {
-        using var serviceScope = serviceProvider.CreateScope();
-        var context = serviceScope.ServiceProvider.GetRequiredService<MainDbContext>();
-
-        await context.Seed(serviceScope);
-    }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseRouting();
+
+app.UseAuthorization();
+var ci = new CultureInfo("en-US");
+
+// Configure the Localization middleware
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(ci),
+    SupportedCultures = new List<CultureInfo>
+        {
+            ci,
+        },
+    SupportedUICultures = new List<CultureInfo>
+        {
+            ci,
+        }
+});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+await SeedDatabase(app.Services);
+app.Run();
+    
+
+static async Task SeedDatabase(IServiceProvider serviceProvider)
+{
+    using var serviceScope = serviceProvider.CreateScope();
+    var context = serviceScope.ServiceProvider.GetRequiredService<MainDbContext>();
+
+    await context.Seed(serviceScope);
+}
+
+public partial class Program { }
